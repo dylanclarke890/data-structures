ce068d158c7135480ac7eec5778139f967a1e3b1
dylanclarke890 <dylanclarke890@gmail.com>
2023-04-07 08:30:32 +0100

    tests: added tests for search methods covered so far

--- src/test/search.test.ts
+++ src/test/search.test.ts
@@ -1,7 +1,23 @@
-test("linear", () => {
-  throw new Error();
+import { binary, linear } from "../core/search";
+
+describe("search_linear", () => {
+  it("returns true when an item is present", () => {
+    const array = [1, 2, 3, 5];
+    expect(linear(array, 3)).toBe(true);
+  });
+  it("returns false when an item is not present", () => {
+    const array = [1, 2, 3, 5];
+    expect(linear(array, 4)).toBe(false);
+  });
 });
 
-test("binary", () => {
-  throw new Error();
+describe("search_binary", () => {
+  it("returns true when an item is present", () => {
+    const array = [1, 2, 3, 5];
+    expect(binary(array, 3)).toBe(true);
+  });
+  it("returns false when an item is not present", () => {
+    const array = [1, 2, 3, 5];
+    expect(binary(array, 4)).toBe(false);
+  });
 });
