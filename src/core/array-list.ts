2c7946d5719c5f1fda88b7fc86610a161ee8a1f1
dylanclarke890 <dylanclarke890@gmail.com>
2023-04-07 11:30:17 +0100

    feat: added enqueue and deque to array-list

--- src/core/array-list.ts
+++ src/core/array-list.ts
@@ -14,14 +14,40 @@ export default class ArrayList<T> {
     return this.items[idx];
   }
 
+  enqueue(item: T): void {
+    if (this.length === this.capacity) {
+      this.increaseCapacity();
+    }
+    for (let i = this.length - 1; i > 0; i--) {
+      this.items[i + 1] = this.items[i];
+    }
+    this.length++;
+    this.items[0] = item;
+  }
+
+  deque(): T | undefined {
+    if (this.length === 0) return undefined;
+    const item = this.items[0];
+    for (let i = 1; i < this.length; i++) {
+      this.items[i - 1] = this.items[i];
+    }
+    this.length--;
+    this.items[this.length] = undefined;
+    return item;
+  }
+
+  private increaseCapacity() {
+    this.capacity = this.capacity * 2;
+    const tmp = this.items;
+    this.items = new Array(this.capacity);
+    for (let i = 0; i < tmp.length; i++) {
+      this.items[i] = tmp[i];
+    }
+  }
+
   push(item: T) {
     if (this.length === this.capacity) {
-      this.capacity = this.capacity * 2;
-      const tmp = this.items;
-      this.items = new Array(this.capacity);
-      for (let i = 0; i < tmp.length; i++) {
-        this.items[i] = tmp[i];
-      }
+      this.increaseCapacity();
     }
     this.items[this.length++] = item;
   }
