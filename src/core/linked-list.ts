0b3ffe474da07e06491f2de6043cb2684135aeee
dylanclarke890 <dylanclarke890@gmail.com>
2023-04-07 09:53:49 +0100

    feat: added interface for linked list

---
+++ src/core/linked-list.ts
@@ -0,0 +1,9 @@
+interface ILinkedList<T> {
+  get length(): number;
+  insertAt(item: T, index: number): void;
+  remove(item: T): T | undefined;
+  removeAt(index: number): T | undefined;
+  append(item: T): void;
+  prepend(item: T): void;
+  get(index: number): T | undefined;
+}
