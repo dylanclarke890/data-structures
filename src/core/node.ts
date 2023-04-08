9cd545035def973e1553c8b7050b5a0cd10223c1
dylanclarke890 <dylanclarke890@gmail.com>
2023-04-08 08:20:25 +0100

    feat: added binary tree node classes

--- src/core/node.ts
+++ src/core/node.ts
@@ -1,18 +1,30 @@
-export class SingleLinkedNode<T> {
+class BaseNode<T> {
   value: T;
-  next?: SingleLinkedNode<T>;
 
   constructor(value: T) {
     this.value = value;
   }
 }
 
-export class DoublyLinkedNode<T> {
-  value: T;
+export class SingleLinkedNode<T> extends BaseNode<T> {
+  next?: SingleLinkedNode<T>;
+}
+
+export class DoublyLinkedNode<T> extends BaseNode<T> {
   next?: DoublyLinkedNode<T>;
   prev?: DoublyLinkedNode<T>;
+}
+
+export class BinaryTreeNode<T> extends BaseNode<T> {
+  left?: BinaryTreeNode<T>;
+  right?: BinaryTreeNode<T>;
+}
+
+export class GeneralTreeNode<T> extends BaseNode<T> {
+  children: BinaryTreeNode<T>[];
 
   constructor(value: T) {
-    this.value = value;
+    super(value);
+    this.children = [];
   }
 }
