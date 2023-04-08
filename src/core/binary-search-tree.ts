fcfa819eda1dfd4bd9775cbbf587bba5ad9e9899
dylanclarke890 <dylanclarke890@gmail.com>
2023-04-08 10:58:56 +0100

    feat: implemented delete for bst

--- src/core/binary-search-tree.ts
+++ src/core/binary-search-tree.ts
@@ -23,8 +23,44 @@ export default class BinarySearchTree {
     this.insertNode(node, this.head);
   }
 
-  delete(value: number) {}
+  delete(value: number): void {
+    this.head = this.deleteNode(this.head, value);
+  }
 
+  private deleteNode(
+    node: BinaryNode<number> | undefined,
+    value: number
+  ): BinaryNode<number> | undefined {
+    if (!node) {
+      return node;
+    }
+
+    if (value < node.value) {
+      node.left = this.deleteNode(node.left, value);
+    } else if (value > node.value) {
+      node.right = this.deleteNode(node.right, value);
+    } else {
+      if (!node.left) {
+        return node.right;
+      } else if (!node.right) {
+        return node.left;
+      }
+
+      node.value = this.findMinValue(node.right);
+      node.right = this.deleteNode(node.right, node.value);
+    }
+
+    return node;
+  }
+
+  private findMinValue(node: BinaryNode<number>): number {
+    let minValue = node.value;
+    while (node.left) {
+      minValue = node.left.value;
+      node = node.left;
+    }
+    return minValue;
+  }
   private insertNode(item: BinaryNode<number>, curr: BinaryNode<number>): void {
     if (item.value <= curr.value) {
       if (curr.left) this.insertNode(item, curr.left);
