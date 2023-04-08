ae274ecd633006ff01605fb21eb13fdd1ea79f9b
dylanclarke890 <dylanclarke890@gmail.com>
2023-04-08 08:57:06 +0100

    fix: moved trees to utils

--- src/__tests__/binary-tree/in-order.ts
+++ src/__tests__/binary-tree/in-order.ts
@@ -1,5 +1,6 @@
 import inOrderSearch from "../../core/binary-tree/in-order";
-import { tree } from "./tree";
+import { tree } from "../utils/tree";
+
 
 test("In order", function () {
   expect(inOrderSearch(tree)).toEqual([5, 7, 10, 15, 20, 29, 30, 45, 50, 100]);
--- src/__tests__/binary-tree/post-order.ts
+++ src/__tests__/binary-tree/post-order.ts
@@ -1,5 +1,5 @@
 import postOrderSearch from "../../core/binary-tree/post-order";
-import { tree } from "./tree";
+import { tree } from "../utils/tree";
 
 test("Post order", function () {
   expect(postOrderSearch(tree)).toEqual([7, 5, 15, 10, 29, 45, 30, 100, 50, 20]);
