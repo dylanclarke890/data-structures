5bf16d1980f1abda51a75337c669b063907f7545
dylanclarke890 <dylanclarke890@gmail.com>
2023-04-08 04:46:59 +0100

    feat: implemented append,prepend, insertAt for doubly linked lists

--- src/core/doubly-linked-list.ts
+++ src/core/doubly-linked-list.ts
@@ -1,3 +1,5 @@
+import { DoublyLinkedNode } from "./node";
+
 interface ILinkedList<T> {
   get length(): number;
   insertAt(item: T, index: number): void;
@@ -8,26 +10,79 @@ interface ILinkedList<T> {
   get(index: number): T | undefined;
 }
 
-export class DoublyLinkedList<TItem> implements ILinkedList<TItem> {
-  get length(): number {
-    throw new Error("Method not implemented.");
+export class DoublyLinkedList<T> implements ILinkedList<T> {
+  length: number;
+  head?: DoublyLinkedNode<T>;
+  tail?: DoublyLinkedNode<T>;
+
+  constructor() {
+    this.length = 0;
   }
-  insertAt(item: TItem, index: number): void {
-    throw new Error("Method not implemented.");
+
+  prepend(item: T): void {
+    const node = new DoublyLinkedNode(item);
+    this.length++;
+    if (!this.head) {
+      this.head = this.tail = node;
+      return;
+    }
+
+    node.next = this.head;
+    this.head.prev = node;
+    this.head = node;
   }
-  remove(item: TItem) {
+
+  append(item: T): void {
+    const node = new DoublyLinkedNode(item);
+    this.length++;
+    if (!this.tail) {
+      this.head = this.tail = node;
+      return;
+    }
+
+    node.prev = this.tail;
+    this.tail.next = node;
+    this.tail = node;
+  }
+
+  insertAt(item: T, index: number): void {
+    if (index > this.length) {
+      throw new Error("Out of bounds id");
+    }
+    if (index === this.length) {
+      this.append(item);
+      return;
+    }
+    if (index === 0) {
+      this.prepend(item);
+      return;
+    }
+
+    let curr = this.head;
+    for (let i = 0; i < index; i++) {
+      curr = this.head?.next;
+    }
+    curr = curr as NonNullable<DoublyLinkedNode<T>>;
+
+    const node = new DoublyLinkedNode(item);
+    node.next = curr;
+    node.prev = curr.prev;
+    curr.next = node;
+    if (curr.prev) {
+      curr.prev.next = node;
+    }
+    this.length++;
+  }
+
+  remove(item: T) {
     return item;
   }
+
   removeAt(index: number) {
-    return null as TItem;
+    return null as T;
   }
-  append(item: TItem): void {
-    throw new Error("Method not implemented.");
-  }
-  prepend(item: TItem): void {
-    throw new Error("Method not implemented.");
-  }
+
   get(index: number) {
-    return null as TItem;
+    return null as T;
   }
 }
