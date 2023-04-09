336a4f45a6d60d94a698e2bcafdcbaf82d2eee24
dylanclarke890 <dylanclarke890@gmail.com>
2023-04-09 03:13:26 +0100

    feat: bfs search on an adjacency matrix

---
+++ src/core/adj-matrix-bfs.ts
@@ -0,0 +1,40 @@
+import Queue from "./queue";
+
+export default function adjMatrixBfs(graph: number[][], source: number, needle: number): number[] {
+  const seen = new Array(graph.length).fill(false);
+  const prev = new Array(graph.length).fill(-1);
+
+  const q = new Queue<number>();
+  seen[source] = true;
+  q.enqueue(source);
+
+  do {
+    const curr = q.deque()!;
+    if (curr === needle) {
+      break;
+    }
+
+    seen[curr] = true;
+    const adjs = graph[curr];
+    for (let i = 0; i < adjs.length; i++) {
+      if (adjs[i] === 0 || seen[i]) continue;
+      seen[i] = true;
+      prev[i] = curr;
+      q.enqueue(i);
+    }
+  } while (q.length);
+
+  if (prev[needle] === -1) {
+    return [];
+  }
+
+  let curr = needle;
+  const out: number[] = [];
+
+  while (prev[curr] !== -1) {
+    out.push(curr);
+    curr = prev[curr];
+  }
+
+  return [source, ...out.reverse()];
+}
