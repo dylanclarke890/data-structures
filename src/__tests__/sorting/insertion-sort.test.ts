9ad4fc3629cee8c01a00e0f50e52e96a65e8c9df
dylanclarke890 <dylanclarke890@gmail.com>
2023-04-09 13:31:41 +0100

    feat: setup for insertion sort

---
+++ src/__tests__/sorting/insertion-sort.test.ts
@@ -0,0 +1,6 @@
+import insertionSort from "../../core/sorting/insertion-sort";
+import sortTest from "../utils/sort-test";
+
+test("Insertion Sort", () => {
+  sortTest(insertionSort);
+});
---
+++ src/core/sorting/insertion-sort.ts
@@ -0,0 +1,3 @@
+export default function insertionSort(array: number[]): number[] {
+  return array;
+}
