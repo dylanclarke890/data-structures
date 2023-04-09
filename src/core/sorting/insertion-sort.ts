e63862b4ae55f3ed992d12596ac489ffed92704c
dylanclarke890 <dylanclarke890@gmail.com>
2023-04-09 13:45:10 +0100

    feat: implemented insertion sort

--- src/core/sorting/insertion-sort.ts
+++ src/core/sorting/insertion-sort.ts
@@ -1,3 +1,16 @@
+/**
+ *
+ * @bigO O(n²) 
+ */
 export default function insertionSort(array: number[]): number[] {
+  for (let i = 1; i < array.length; i++) {
+    const key = array[i];
+    let j = i - 1;
+    while (j >= 0 && array[j] > key) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = key;
+  }
   return array;
 }
