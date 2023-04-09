5d206ea211efd86d48520d0c75d7cd37bbbc905c
dylanclarke890 <dylanclarke890@gmail.com>
2023-04-09 13:56:23 +0100

    docs: for insertion sort

--- src/core/sorting/insertion-sort.ts
+++ src/core/sorting/insertion-sort.ts
@@ -1,6 +1,10 @@
 /**
- *
- * @bigO O(n²) 
+ * Processes elements one at a time, comparing them with the elements to the left of them (which is also the reason
+ * we start processing from the 2nd element onwards). Works best on small arrays or arrays that are already partially
+ * sorted.
+ * @bigOBest O(n)
+ * @bigOAverage O(n²)
+ * @bigOWorst O(n²)
  */
 export default function insertionSort(array: number[]): number[] {
   for (let i = 1; i < array.length; i++) {
