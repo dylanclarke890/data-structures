ee9242563c02168c7feb65f765a236fa6d5f2188
dylanclarke890 <dylanclarke890@gmail.com>
2023-04-09 09:00:43 +0100

    feat: add tests for dijkstra

---
+++ src/__tests__/djikstra.test.ts
@@ -0,0 +1,6 @@
+import dijkstraList from "../core/djikstra";
+import { list1 } from "./utils/graph";
+
+test("dijkstra via adj list", function () {
+  expect(dijkstraList(0, 6, list1)).toEqual([0, 1, 4, 5, 6]);
+});
