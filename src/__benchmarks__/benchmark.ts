88cc465e68b2fb5ba9974a39c320612d8df1f37d
dylanclarke890 <dylanclarke890@gmail.com>
2023-04-10 19:00:16 +0100

    feat: condensed files into one

--- src/__benchmarks__/benchmark.ts
+++ src/__benchmarks__/benchmark.ts
@@ -1,7 +1,33 @@
-import bubbleSortBenchmark from "./sorting/bubble-sort.benchmark";
+import bubbleSort from "../core/sorting/bubble-sort";
+import timeFunction from "./utils/time-function";
 
-const benchmarks = [bubbleSortBenchmark];
+let arrays: number[][];
+const benchmarks = [bubbleSort];
 
-for (let i = 0; i < benchmarks.length; i++) {
-  benchmarks[i]();
+(function benchmark() {
+  benchmarks.forEach((b) => {
+    setup();
+    warmUp();
+    arrays.forEach((a) => {
+      timeFunction(`Bubble Sort (${a.length})`, () => {
+        b(a);
+      });
+    });
+  });
+})();
+
+function createArrays(): number[][] {
+  return [10, 100, 1000, 10_000, 100_000, 500_000].map((size) =>
+    Array.from({ length: size }, () => Math.floor(Math.random()) * size)
+  );
+}
+
+function setup() {
+  arrays = createArrays();
+}
+
+function warmUp() {
+  for (let i = 0; i < 100; i++) {
+    bubbleSort([4, 6, 2]);
+  }
 }
